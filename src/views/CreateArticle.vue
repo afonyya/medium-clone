84a24f5ab06bc6ac9f86e27692e458078854f0ab
Alexandr Afonichev <afonyaafona@gmail.com>
2021-04-30 16:35:30 +0300

    added article form

---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div>
+    CreateArticle
+    <ArticleForm
+      :initial-values="initialValues"
+      :errors="validationErrors"
+      :is-submitting="isSubmitting"
+      @articleSubmit="onSubmit"
+    />
+  </div>
+</template>
+
+<script>
+import ArticleForm from '@/components/ArticleForm.vue'
+
+export default {
+  name: 'CreateArticle',
+  components: {
+    ArticleForm
+  },
+  data () {
+    return {
+      initialValues: {
+        title: '',
+        description: '',
+        body: '',
+        tagList: []
+      },
+      validationErrors: null,
+      isSubmitting: false
+    }
+  },
+  methods: {
+    onSubmit (data) {
+      console.log('submit', data)
+    }
+  }
+}
+</script>
